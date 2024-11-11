57588aa379472a63f38763df24d82d75fe806a1c
yay <61081228+yanganyi@users.noreply.github.com>
2024-11-11 10:12:32 +0800

    Initial Commit

---
+++ finalproj/finalprojApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalprojApp.swift
+//  finalproj
+//
+//  Created by Yang An Yi on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct finalprojApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ finalprojTests/finalprojTests.swift
@@ -0,0 +1,17 @@
+//
+//  finalprojTests.swift
+//  finalprojTests
+//
+//  Created by Yang An Yi on 11/11/24.
+//
+
+import Testing
+@testable import finalproj
+
+struct finalprojTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
